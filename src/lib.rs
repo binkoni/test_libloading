79e76ff5da24c0e338662b2a85cb030a89568f76
Byeonggon Lee <gonny952@gmail.com>
2018-03-28 18:48:25 +0900

    RTLD_NODELETE works

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,13 @@ extern crate libloading;
 
 #[test]
 fn libloading() {
+    /*
+    //This code fix the problem
+    const RTLD_NOW: std::os::raw::c_int = 0x00002;
+    const RTLD_NODELETE: std::os::raw::c_int = 0x01000;
+    let os_lib = libloading::os::unix::Library::open(Some("libdltest.so"), RTLD_NOW | RTLD_NODELETE).unwrap();
+    let lib = libloading::Library::from(os_lib);
+    */
     let lib = libloading::Library::new("libdltest.so").unwrap();
     unsafe {
         let test_fn: libloading::Symbol<unsafe extern fn(i32) -> i32> = lib.get(b"test").unwrap();
